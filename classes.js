4adc1b863e30cf7215be019625e1fcfffb3ec8a3
Derek <levidvi@gmail.com>
2020-05-03 16:56:06 +1200

    elements all visible and 3 coloured- complete

--- classes.js
+++ classes.js
@@ -7,28 +7,32 @@ function start() {
 one();
 two();
 three();
-
+makeVisible();
 }
 
 function one() {
   // First, we have to find the element:
-  var one = document.getElementById('one')
-
+  var one = document.getElementById('one');
   // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+  one.classList.add('blue');
 }
 
 // CREATE FUNCTION two HERE
 function two() {
-  var two = document.getElementById('two')
-  one.classList.add('green') 
+  var two = document.getElementById('two');
+  two.classList.add('green') ;
 }
 
 // CREATE FUNCTION three HERE
 function three () { 
 
-  var three = document.getElementById('three')
-  one.classList.add('purple')
+  var three = document.getElementById('three');
+  three.classList.add('purple');
 }
 
+function makeVisible() {
+ var invisibleElements = document.getElementsByClassName('invisible');
+console.log(invisibleElements[0]);
+invisibleElements[0].classList.add('visible');
+}
 // CREATE FUNCTION makeVisible HERE
